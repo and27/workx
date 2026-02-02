a1de1054b0378648ce58dbd8e7a6d6da3cd6fe8c
Andrés Banda <andrezh_gln7@live.com>
2026-02-02 14:53:10 -0500

    fix: allow toast title and description

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -110,8 +110,12 @@ const ToastDescription = React.forwardRef<
 ));
 ToastDescription.displayName = ToastPrimitives.Description.displayName;
 
-type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>;
 type ToastActionElement = React.ReactElement<typeof ToastAction>;
+type ToastProps = React.ComponentPropsWithoutRef<typeof Toast> & {
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: ToastActionElement;
+};
 
 export {
   ToastProvider,
